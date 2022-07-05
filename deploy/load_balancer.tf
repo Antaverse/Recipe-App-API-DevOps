cdb589da9d3df7e5bf9f262a17d0f8f430cf76d0
Eddie Khor <eddie.khor@theantpire.onmicrosoft.com>
2022-07-05 21:08:18 +0800

    kjd

--- deploy/load_balancer.tf
+++ deploy/load_balancer.tf
@@ -32,18 +32,7 @@ resource "aws_lb_listener" "api" {
     target_group_arn = aws_lb_target_group.api.arn
   }
 }
-resource "aws_lb_listener" "api_https" {
-  load_balancer_arn = aws_lb.api.arn
-  port              = 443
-  protocol          = "HTTPS"
 
-  certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.api.arn
-  }
-}
 
 resource "aws_security_group" "lb" {
   description = "Allow access to application load balancer"
